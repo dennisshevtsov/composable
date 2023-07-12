a93dfd48473b0d4fcccf96e9468f85a122d71ba0
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-07-12 12:48:59 +0300

    add patchable dto

---
+++ sample/Patchable.Sample/PatchBookRequestDto.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Patchable.Sample;
+
+public sealed class PatchBookRequestDto : IPatchable
+{
+  public PatchBookRequestDto()
+  {
+    Title       = string.Empty;
+    Description = string.Empty;
+    Pages       = 0;
+    Properties  = Array.Empty<string>();
+  }
+
+  public string Title { get; set; }
+
+  public string Description { get; set; }
+
+  public int Pages { get; set; }
+
+  public string[] Properties { get; set; }
+}
