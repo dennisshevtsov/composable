0904331bd1a1544f800e6359f1ff6410488bf163
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-07-12 13:15:48 +0300

    add book controller

---
+++ sample/Patchable.Sample/BookController.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Patchable.Sample;
+
+[Route("book")]
+[ApiController]
+public sealed class BookController : ControllerBase
+{
+  [HttpPatch("{bookId}", Name = nameof(BookController.PatchBook))]
+  public IActionResult PatchBook(PatchBookRequestDto requestDto)
+  {
+    return Ok();
+  }
+}
