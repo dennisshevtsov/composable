466bb0532b8b8659c89c7c2bb576707316242613
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-07-17 22:21:19 +0300

    add test for patchable model binder

--- test/Patchable.Test/PatchableModelBinderTest.cs
+++ test/Patchable.Test/PatchableModelBinderTest.cs
@@ -2,6 +2,10 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Moq;
+
 namespace Patchable.Test;
 
 [TestClass]
@@ -15,5 +19,20 @@ public sealed class PatchableModelBinderTest
   public void Initialize()
   {
     _patchableModelBinder = new PatchableModelBinder();
+  }
+
+  [TestMethod]
+  public async Task BindModelAsync_NoBody_SetSuccessResult()
+  {
+    // Arrange
+    Mock<ModelBindingContext> modelBindingContextMock = new();
+    modelBindingContextMock.SetupSet(context => context.Result = It.IsAny<ModelBindingResult>())
+                           .Verifiable();
+
+    // Act
+    await _patchableModelBinder.BindModelAsync(modelBindingContextMock.Object);
+
+    // Assert
+    modelBindingContextMock.VerifySet(context => context.Result = It.Is<ModelBindingResult>(result => result.IsModelSet));
   }
 }
