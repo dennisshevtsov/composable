3fa7dc608b584725fa64df8d2d8d30f43e5e2018
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-07-24 18:25:01 +0300

    add composable model binder

---
+++ src/Patchable/ComposableModelBinder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Patchable;
+
+/// <summary>
+/// The <see cref="ComposableModelBinder"/> provides a mechanism to create an instance
+/// of a model that implements the <see cref="IComposable"/> from HTTP request. The binder
+/// allows to bind parameters from the body, the route and the query string of the HTTP.
+/// </summary>
+public sealed class ComposableModelBinder : IModelBinder
+{
+  /// <summary>
+  /// Attempts to bind a model.
+  /// </summary>
+  /// <param name="bindingContext">A context that contains operating information for model binding and validation.</param>
+  /// <returns>An instance of the <see cref="Task"/> that represents an asynchronous operation.</returns>
+  public async Task BindModelAsync(ModelBindingContext bindingContext)
+  {
+    object model = Activator.CreateInstance(bindingContext.ModelType)!;
+
+    bindingContext.Result = ModelBindingResult.Success(model);
+  }
+}
