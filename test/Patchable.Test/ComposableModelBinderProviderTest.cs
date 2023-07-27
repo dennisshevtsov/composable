7c010b01597d4db86a0009b37c1e5d8579579732
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-07-27 23:29:30 +0300

    add composite model binder provider test

---
+++ test/Patchable.Test/ComposableModelBinderProviderTest.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using Moq;
+
+namespace Patchable.Test;
+
+[TestClass]
+public sealed class ComposableModelBinderProviderTest
+{
+}
