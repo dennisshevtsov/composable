e27a971b72d9ab2aa9fd759742988b698d9bbb9f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-07-29 18:23:07 +0300

    override bindmodelasync

--- src/Patchable/PatchableModelBinder.cs
+++ src/Patchable/PatchableModelBinder.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
 namespace Patchable;
 
 /// <summary>
@@ -12,4 +14,18 @@ namespace Patchable;
 /// </summary>
 public sealed class PatchableModelBinder : ComposableModelBinder
 {
+  public async override Task BindModelAsync(ModelBindingContext bindingContext)
+  {
+    object model = Activator.CreateInstance(bindingContext.ModelType)!;
+
+    Dictionary<string, ModelMetadata> metadata = GetPropertyMetadata(bindingContext);
+    Dictionary<string, object?> values = await GetPropertyValuesAsync(metadata, bindingContext);
+
+    foreach (var value in values)
+    {
+      metadata[value.Key].PropertySetter!.Invoke(model, value.Value);
+    }
+
+    bindingContext.Result = ModelBindingResult.Success(model);
+  }
 }
