286a28ab470110d9050c7d822de79745425452a3
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-01 12:36:59 +0300

    add copyrightsg

--- sample/Patchable.Sample/Book/PutBookRequestDto.cs
+++ sample/Patchable.Sample/Book/PutBookRequestDto.cs
@@ -1,3 +1,7 @@
-﻿namespace Patchable.Sample.Book;
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Patchable.Sample.Book;
 
 public record PutBookRequestDto(Guid BookId, string Title, string Description, string[] Authors);
