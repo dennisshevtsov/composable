d5ef7a2ff91bf9563b38c1b24cc77f9bc7233545
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-01 12:45:51 +0300

    add book service

---
+++ sample/Patchable.Sample/Book/BookService.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Patchable.Sample.Book;
+
+public sealed class BookService
+{
+  private readonly Dictionary<Guid, BookEntity> _books = new();
+
+  public BookEntity? GetBook(Guid bookId) =>
+    _books.TryGetValue(bookId, out BookEntity? book) ? book : null;
+
+  public void SaveBook(BookEntity book) => _books[book.BookId] = book;
+}
