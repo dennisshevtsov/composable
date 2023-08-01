e29dacf7e3f70e94d3369129f994b4975d1daf4b
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-01 12:46:22 +0300

    register book service

--- sample/Patchable.Sample/Program.cs
+++ sample/Patchable.Sample/Program.cs
@@ -2,8 +2,11 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using Patchable.Sample.Book;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(options => options.AddPatchable());
+builder.Services.AddScoped<BookService>();
 
 WebApplication app = builder.Build();
 app.Run();
