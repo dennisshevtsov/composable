cb2f734c9322fd9d589db51cdb98b66eaffae31f
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-01 12:23:00 +0300

    add get dtos

---
+++ sample/Patchable.Sample/Book/GetBookResponseDto.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Dennis Shevtsov. All rights reserved.
+// Licensed under the MIT License.
+// See LICENSE in the project root for license information.
+
+namespace Patchable.Sample.Book;
+
+public sealed class GetBookResponseDto
+{
+  public GetBookResponseDto(
+    Guid bookId,
+    string title,
+    string description,
+    string[] authors)
+  {
+    BookId = bookId;
+    Title  = title;
+    Description = description;
+    Authors = authors;
+  }
+
+  public Guid BookId { get; }
+
+  public string Title { get; }
+
+  public string Description { get; }
+
+  public string[] Authors { get; }
+}
