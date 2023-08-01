75057de978c4762d4215c76911c2c7148076cd79
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-01 12:37:27 +0300

    add actions

--- sample/Patchable.Sample/Book/BookController.cs
+++ sample/Patchable.Sample/Book/BookController.cs
@@ -10,8 +10,32 @@ namespace Patchable.Sample.Book;
 [ApiController]
 public sealed class BookController : ControllerBase
 {
+  [HttpGet("{bookId}", Name = nameof(BookController.GetBook))]
+  public IActionResult GetBook(GetBookRequestDto requestDto)
+  {
+    return Ok();
+  }
+
+  [HttpPost(Name = nameof(BookController.PostBook))]
+  public IActionResult PostBook(PostBookRequestDto requestDto)
+  {
+    return Ok();
+  }
+
+  [HttpPut("{bookId}", Name = nameof(PutBook))]
+  public IActionResult PutBook(PutBookRequestDto requestDto)
+  {
+    return Ok();
+  }
+
   [HttpPatch("{bookId}", Name = nameof(PatchBook))]
   public IActionResult PatchBook(PatchBookRequestDto requestDto)
+  {
+    return Ok();
+  }
+
+  [HttpDelete("{bookId}", Name = nameof(DeleteBook))]
+  public IActionResult DeleteBook(DeleteBookRequestDto requestDto)
   {
     return Ok();
   }
