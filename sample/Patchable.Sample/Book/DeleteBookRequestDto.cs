2c044a3fa5b2eeabc5a3f2e0de769d46aafd2dec
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-04 21:56:50 +0300

    fix delete

--- sample/Patchable.Sample/Book/DeleteBookRequestDto.cs
+++ sample/Patchable.Sample/Book/DeleteBookRequestDto.cs
@@ -4,4 +4,7 @@
 
 namespace Patchable.Sample.Book;
 
-public record DeleteBookRequestDto(Guid BookId) : IComposable;
+public record DeleteBookRequestDto(Guid BookId) : IComposable
+{
+  public DeleteBookRequestDto() : this(Guid.Empty) { }
+}
