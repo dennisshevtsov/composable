18a281e8592dd5901be818b11fd2a527d17be902
Dennis Shevtsov <dnnsshvtsv@gmail.com>
2023-08-07 20:23:10 +0300

    add validation to post

--- sample/Patchable.Sample/Book/PostBookRequestDto.cs
+++ sample/Patchable.Sample/Book/PostBookRequestDto.cs
@@ -2,11 +2,20 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Patchable.Sample.Book;
 
-public record class PostBookRequestDto(string Title, string Description, string[] Authors) : IComposable
+public record class PostBookRequestDto : IComposable
 {
-  public PostBookRequestDto() : this(string.Empty, string.Empty, Array.Empty<string>()) { }
+  [Required, MinLength(1), MaxLength(255)]
+  public string Title { get; set; } = string.Empty;
+
+  [MaxLength(255)]
+  public string Description { get; set;} = string.Empty;
+
+  [Required, MinLength(1)]
+  public string[] Authors { get; set; } = Array.Empty<string>();
 
   internal BookEntity ToBookEntity() => new(Guid.NewGuid(), Title, Description, Authors);
 }
